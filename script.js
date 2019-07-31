c2ce2e11c990b4aa32c35c6c446f44573798e3da
Sabry <Neamat.Sabry@thomsonreuters.com>
2019-07-31 16:54:23 -0500

    new background colors plus cleaned up some html

--- script.js
+++ script.js
@@ -25,9 +25,14 @@ bot.addEventListener("click", function () {
   
     if (x.style.animationPlayState === "paused") {
         x.style.animationPlayState = "running";
+
+        this.firstChild.classList.add("fa-pause");
+        this.firstChild.classList.remove("fa-play");
     } else {
         x.style.animationPlayState = "paused";
-      //box.style.animationPlayState = "paused"
+
+        this.firstChild.classList.remove("fa-pause");
+        this.firstChild.classList.add("fa-play");
     }
 
 });
